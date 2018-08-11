c337bafb1d915c8b83c7e375391c9f6e5e4ef201
Alexis Mendoza Valencia <A01631994@itesm.mx>
2018-08-11 17:32:30 -0500

    Update Kata_1.py

--- Kata_1.py
+++ Kata_1.py
@@ -1,4 +1,31 @@
-"""Find the Smallest Number in a List
+"""
+Excercise 1: Is the Number Even or Odd?
+
+Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers.
+Examples
+
+3 ➞ "odd"
+
+146 ➞ "even"
+
+19 ➞ "odd"
+
+Notes
+
+    Dont forget to return the result.
+    Input will always be a valid integer.
+    Expect negative integers (whole numbers).
+    Tests are case sensitive (return "even" or "odd" in lowercase).
+    
+"""
+    
+#----------------------------Code------------------------------------
+def isEvenOrOdd(num):
+	if num%2 == 0: return "even"
+	else: return "odd"
+
+"""
+Excercise 2: Find the Smallest Number in a List
 
 Create a function that takes a list of numbers and returns the smallest number in the list.
 Examples
@@ -17,8 +44,31 @@ Notes
 
     Test cases contain decimals.
     If you get stuck on a challenge, find help in the Resources tab.
-    If you're really stuck, unlock solutions in the Solutions tab."""
+    If you're really stuck, unlock solutions in the Solutions tab.
     
+"""
 #----------------------------Code------------------------------------
 def findSmallestNum(lst):
 	return min(lst)
+
+"""
+Excercise 3: Eliminate Odd Numbers within a List
+
+Create a function that takes a list of numbers and returns only the even values.
+Examples
+
+[1, 2, 3, 4, 5, 6, 7, 8] ➞ [2, 4, 6, 8]
+
+[43, 65, 23, 89, 53, 9, 6] ➞ [6]
+
+[718, 991, 449, 644, 380, 440] ➞ [718, 644, 380, 440]
+
+Notes
+
+    Return all even numbers in the order they were given.
+    All test cases contain valid numbers ranging from 1 to 3000.
+"""
+#----------------------------Code------------------------------------
+def noOdds(lst):
+	newList = [i for i in lst if i%2 == 0]
+	return newList
