b02c25f2859c8f589d1c3811f571da7613d92c79
Alexis Mendoza Valencia <A01631994@itesm.mx>
2018-08-28 19:28:53 -0500

    Create Kata_5.py

---
+++ Kata_5.py
@@ -0,0 +1,22 @@
+"""
+Ejercicio 1: 
+Return the Sum of the Two Smallest Numbers
+
+Create a function that takes a list of numbers and returns the sum of the two lowest positive numbers.
+Examples
+
+[19, 5, 42, 2, 77] ➞ 7
+
+[10, 343445353, 3453445, 3453545353453] ➞ 3453455
+
+[2, 9, 6, -1] ➞ 8
+
+[879, 953, 694, -847, 342, 221, -91, -723, 791, -587] ➞ 563
+
+[3683, 2902, 3951, -475, 1617, -2385] ➞ 4519
+
+Notes
+
+    Don't count negative numbers.
+    Floats and empty lists will not be used in any of the test cases.
+"""
